a0acd29d14d280683396ffb15e6775f5ec575ca4
T0mer007 <tomerbenaim1@gmail.com>
2023-05-22 21:57:29 +0300

    backend connected

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -18,59 +18,27 @@ export const toyService = {
 
 
 function query(filterBy = {}, sortBy = '') {
-    return asyncStorageService.query(STORAGE_KEY)
-        .then((toys) => {
-            let toysToDisplay = toys
-            if (filterBy.inStock) {
-                toysToDisplay = toys.filter(toy => toy.inStock)
-            }
-            if (filterBy.search) {
-                const regExp = new RegExp(filterBy.search, 'i')
-                toysToDisplay = toys.filter(toy => regExp.test(toy.name))
-                console.log('toysToDisplay: ', toysToDisplay )
-            }
-            if (sortBy === 'createdAt') toysToDisplay = toys.sort((a, b) => a.createdAt - b.createdAt)
-            if (sortBy === 'price') toysToDisplay = toys.sort((a, b) => a.price - b.price)
-            if (sortBy === 'name') toysToDisplay = toys.sort((a, b) => {
-                if (a.name.toUpperCase() < b.name.toUpperCase()) return -1
-                else return 1
-            })
-            return Promise.resolve(toysToDisplay)
-            //     // if (filterBy.pageIdx !== undefined) {
-            //     //     let startIdx = filterBy.pageIdx * PAGE_SIZE
-            //     //     toysToDisplay = toys.slice(startIdx, startIdx + PAGE_SIZE)
-
-            //     // }
-
-
-        })
-
-    // return httpService.get(BASE_URL, filterBy)
+    // return asyncStorageService.query(STORAGE_KEY)
+    return httpService.get(BASE_URL, {filterBy ,sortBy})
 }
 function getById(toyId) {
-    // return httpService.get(BASE_URL + toyId)
-    return asyncStorageService.get(STORAGE_KEY, toyId)
+    return httpService.get(BASE_URL + toyId)
+    // return asyncStorageService.get(STORAGE_KEY, toyId)
 }
 function remove(toyId) {
-    return asyncStorageService.remove(STORAGE_KEY, toyId)
+    // return asyncStorageService.remove(STORAGE_KEY, toyId)
     // return Promise.reject('Not now!')
-    // return httpService.delete(BASE_URL + toyId)
+    return httpService.delete(BASE_URL + toyId)
 }
 function save(toy) {
+    // const method = toy._id ? 'put' : 'post'
+    // return asyncStorageService[method](STORAGE_KEY, toy)
+    if (toy._id) {
+        return httpService.put(BASE_URL, toy)
+    } else {
 
-    const method = toy._id ? 'put' : 'post'
-    return asyncStorageService[method](STORAGE_KEY, toy)
-
-
-
-    // if (toy._id) {
-    //     return httpService.put(BASE_URL, toy)
-    // } else {
-
-    //     return httpService.post(BASE_URL, toy)
-    // }
-
-
+        return httpService.post(BASE_URL, toy)
+    }
 }
 
 function getEmptyToy() {
